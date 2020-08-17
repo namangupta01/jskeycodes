9012a88a9988b2e4b0c4382738bbd565366190bd
Naman Gupta <01namangupta@gmail.com>
2020-08-17 12:37:24 +0530

    Minor Fixes

--- script.js
+++ script.js
@@ -7,9 +7,5 @@ function keyCodesGenerator(e){
 	code=e.keyCode;
 	codeBox.innerText= code+"";
 	buttonPressed.innerText=e.code+"";
-	buttonPressed.style.paddingLeft=20+"px"
-	buttonPressed.style.paddingRight=20+"px"
-	buttonPressed.style.paddingTop=5+"px"
-	buttonPressed.style.paddingBottom=5+"px"
 }
 document.addEventListener("keydown",keyCodesGenerator);
